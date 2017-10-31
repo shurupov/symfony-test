8b2f0e2aace58ba9f81dd3c3f5892fc8b4d4ca10
Evgeny Shurupov <bolteg86@ya.ru>
2017-10-31 14:23:32 +0400

    Добавил файлы проекта

--- scripts/soft/symfony-test.sh
+++ scripts/soft/symfony-test.sh
@@ -6,6 +6,10 @@ sudo cp vhosts/symfony-test.conf /etc/apache2/sites-available
 sudo ln -s /etc/apache2/sites-available/symfony-test.conf /etc/apache2/sites-enabled
 sudo service apache2 restart #todo move this to another place
 
+sudo mkdir -p /usr/local/bin
+sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
+sudo chmod a+x /usr/local/bin/symfony
+
 sudo chmod 777 /var/www/html/symfony-test -R
 cd /var/www/html/symfony-test
 composer install
---
+++ symfony-test/README.md
@@ -0,0 +1,4 @@
+.
+=
+
+A Symfony project created on October 31, 2017, 10:18 am.
